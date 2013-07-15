218a11793c9c0369bd021c4823e72ba19325234c
travisl12 <travis.lawrence12@gmail.com>
2013-07-14 21:54:15 -0700

    WIP

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
   def index
     @yoda_img = Dir.entries('app/assets/images')[4..-1].sample
-    @quotes = Quote.all.map(&:quote).sample
+    @quotes = Quote.all.sample
   end
 
 end
